4feb7ca5d13b036bd958a37cda6e1c22c4f63718
Mestrada1997 <125697853+Mestrada1997@users.noreply.github.com>
2023-04-13 12:40:30 -0500

    Add files via upload

---
+++ README.md-HW2.py
@@ -0,0 +1,34 @@
+## Loan Qualifier Application
+This is a python command interface which allows users to see qualifying loans from lenders. How the application works
+is by taking in the daily_rate sheet of loan criteria for various loan providers through asking the user several 
+questions to evaluate their loan eligibility and returns a list of qualifying loans 
+
+
+## Technologies 
+These were the programs that were imported. 
+
+* Fire
+* Questionary 
+
+
+
+## Installation Guide 
+
+To start on this project these programs need to be installed 
+...python
+pip install fire 
+pip install Questionary 
+
+
+----
+## Usage 
+To use loan qualifier application, clone the repository and run the **app.py** with the following: 
+
+...python 
+python app.py
+
+## Contributors 
+
+
+
+##License 
